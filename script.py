f37f8116b305e52240d60defacdceab0ab8237c7
Ken Hileman <ken.hileman@outlook.com>
2024-02-11 15:55:42 -0500

    first commit

---
+++ script.py
@@ -0,0 +1,15 @@
+def celsius_to_fahrenheit(t):
+    '''Temperature Conversion Function'''
+    fahrenheit = t * (9/5) + 32
+    return fahrenheit
+
+print(celsius_to_fahrenheit(1))
+print(celsius_to_fahrenheit(4))
+print(celsius_to_fahrenheit(-4))
+print(celsius_to_fahrenheit(-1))
+print(celsius_to_fahrenheit(-3))
+print(celsius_to_fahrenheit(3))
+print(celsius_to_fahrenheit(2))
+print(celsius_to_fahrenheit(0))
+print(celsius_to_fahrenheit(-5))
+print(celsius_to_fahrenheit(-2))
